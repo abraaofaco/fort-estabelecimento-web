e55a48d2d99324869ee27bd5515156e9eb69b0d1
Abraão Facó <abraaofaco@gmail.com>
2021-03-21 16:08:24 -0300

    Add litagem dos estabelecimentos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,72 @@
 import React from "react";
-import Header from "../../components/Header";
+import { useState } from "react";
+import { useEffect } from "react";
+import { IoTrashBin } from "react-icons/io5";
+import { RiEditBoxFill } from "react-icons/ri";
 import MasterPage from "../../components/MasterPage";
+import api from "../../services/api";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Establishment,
+  Addresse,
+  InfoAddresse,
+  ActionsAddresse,
+} from "./styles";
+
+interface IAddresse {
+  id: string;
+  establishmentId: string;
+  addresse: string;
+  number: string;
+  district: string;
+  city: string;
+  state: string;
+  country: string;
+}
+
+interface IEstablishment {
+  id: string;
+  name: string;
+  addresses: IAddresse[];
+}
 
 const Home: React.FC = () => {
+  const [establishments, setEstablishments] = useState<IEstablishment[]>([]);
+
+  useEffect(() => {
+    api.get("/establishments").then((response) => {
+      setEstablishments(response.data);
+    });
+  }, []);
+
   return (
     <MasterPage>
       <Container>
         <h2>Estabelecimentos</h2>
+        {establishments.map((establishment) => (
+          <div key={establishment.id}>
+            <Establishment>
+              <h4>{establishment.name}</h4>
+            </Establishment>
+            {establishment.addresses.map((addresse) => (
+              <Addresse key={addresse.id}>
+                <InfoAddresse>
+                  <span>{`${addresse.addresse}, ${addresse.number}, ${addresse.district}`}</span>
+                  <span>{`${addresse.city}, ${addresse.state}/${addresse.country}`}</span>
+                </InfoAddresse>
+                <ActionsAddresse>
+                  <button type="button">
+                    <RiEditBoxFill size={25} color="#378fbf" />
+                  </button>
+                  <button type="button">
+                    <IoTrashBin size={25} color="#c53030" />
+                  </button>
+                </ActionsAddresse>
+              </Addresse>
+            ))}
+          </div>
+        ))}
       </Container>
     </MasterPage>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,44 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  > h2 {
+    color: #ff9000;
+  }
+`;
+
+export const Establishment = styled.div`
+  display: flex;
+  flex: row;
+  align-items: center;
+  margin-top: 20px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const Addresse = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px 5px;
+  margin-top: 10px;
+`;
+
+export const InfoAddresse = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #999;
+`;
+
+export const ActionsAddresse = styled.div`
+  button {
+    background: none;
+    border: none;
+
+    & + button {
+      margin-left: 10px;
+    }
+  }
+`;
